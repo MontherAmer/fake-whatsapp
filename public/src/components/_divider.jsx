32d7bc1e3cd873e865de1f493be5a844419492a3
montheramer <monther.amer48@gmail.com>
2021-06-05 20:47:50 +0300

    CREATE: divider component

---
+++ public/src/components/_divider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+  return (
+    <div class="mx-auto my-2 small py-1 px-2 rounded" id="divider">
+      25/03/2018
+    </div>
+  );
+};
+
+export default index;
