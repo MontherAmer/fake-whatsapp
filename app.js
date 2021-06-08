1937a3f0cb16296ee9859c5e654c5d2105dea822
montheramer <monther.amer48@gmail.com>
2021-06-08 15:43:35 +0300

    ADD cors and env and static file response

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
+
+require('dotenv').config();
 
 const indexRouter = require('./routes');
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.use('/', indexRouter);
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/build/index.html')));
 
 module.exports = app;
