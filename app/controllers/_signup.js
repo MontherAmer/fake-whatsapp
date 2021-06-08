ffe3d35f5e6f9e3f601842cd7477968361a1c7a3
montheramer <monther.amer48@gmail.com>
2021-06-08 18:58:02 +0300

    CREATE: signup function

---
+++ app/controllers/_signup.js
@@ -0,0 +1,19 @@
+const { User } = require('../models');
+const { createJWT } = require('../utils');
+
+exports.create = async (req, res) => {
+  try {
+    let { name, email, password } = req.body;
+    if (!email || !name || !password) return res.send({ success: false, status: 400, message: 'missing fields' });
+
+    let user = new User({ name, email, password });
+
+    user = await user.save();
+    ({ _id, email } = user);
+    let token = createJWT({ _id, email });
+
+    return res.send({ success: true, status: 200, data: { email, name, _id, token } });
+  } catch (err) {
+    return res.send({ success: false, status: 500 });
+  }
+};
