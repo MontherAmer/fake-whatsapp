d8a9134ae4ac0f0a4eb6ef2687ea47711a530998
montheramer <monther.amer48@gmail.com>
2021-06-09 18:57:19 +0300

    CREATE: /friend API

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const { isAuthenticated } = require('../app/utils');
 const controllers = require('../app/controllers');
 
 router.post('/signup', controllers.signup);
 
 router.post('/login', controllers.login);
+
+router.post('/friend', isAuthenticated, controllers.friend);
 
 module.exports = router;
