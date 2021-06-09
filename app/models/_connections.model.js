817ee700a8b63bf8abd70dbbd5e325153fb85bf6
montheramer <monther.amer48@gmail.com>
2021-06-09 18:50:36 +0300

    UPDATE: models

--- app/models/_connections.model.js
+++ app/models/_connections.model.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+const { v4: uuid } = require('uuid');
 
 const ConnectionSchema = new mongoose.Schema(
   {
+    uuid: { type: String, default: uuid },
+    name: { type: String },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     type: { type: String, enum: ['User', 'Group'] },
     messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
