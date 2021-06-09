5018cc8c5915310502753b094928053d25a88f76
montheramer <monther.amer48@gmail.com>
2021-06-09 18:52:00 +0300

    UPDATE: loader component

--- public/src/components/_loader.jsx
+++ public/src/components/_loader.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 const Loader = () => {
   const { loader } = useSelector((state) => state.utilsState);
 
+  useEffect(() => {}, [loader]);
+
   return (
-    <div id="startup" style={{ display: loader ? 'fixed' : 'none' }}>
+    <div id="startup" style={{ display: loader ? 'flex' : 'none' }}>
       <svg class="spinner-container" width="65px" height="65px" viewBox="0 0 52 52">
         <circle class="path" cx="26px" cy="26px" r="20px" fill="none" stroke-width="4px" />
       </svg>
