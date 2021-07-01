ca7da9e34cbf12a43626cfb485ce27c12cc0d07d
montheramer <monther.amer48@gmail.com>
2021-07-01 17:05:26 +0300

    handle remove input after sending message

--- public/src/components/_input.jsx
+++ public/src/components/_input.jsx
@@ -6,7 +6,7 @@ import { SocketContext } from '../context/SocketContext';
 import Emoji from './_emoji';
 
 const Input = ({ id }) => {
-  const [state, setState] = useState({});
+  const [state, setState] = useState({ message: '' });
 
   const { submitMsg } = useContext(SocketContext);
 
@@ -18,7 +18,7 @@ const Input = ({ id }) => {
 
   const toggleEmoji = () => setState({ ...state, show: !state.show });
 
-  const submit = () => submitMsg({ text: state.message, to: id });
+  const submit = () => (submitMsg({ text: state.message, to: id }), setState({ ...state, message: '', show: false }));
 
   return (
     <div className="justify-self-end align-items-center flex-row d-flex position-relative" id="input-area">
@@ -32,7 +32,7 @@ const Input = ({ id }) => {
         onChange={handleChange}
         className="flex-grow-1 border-0 px-3 py-2 my-3 shadow-sm"
       />
-      <i className="fas fa-paper-plane text-muted px-3" style={{ cursor: 'pointer' }} onClick={submit} />
+      <i className="fas fa-paper-plane text-muted px-3" style={{ cursor: 'pointer' }} onClick={state.message.length ? submit : null} />
       <Emoji handleChoose={handleEmoji} show={state.show} />
     </div>
   );
